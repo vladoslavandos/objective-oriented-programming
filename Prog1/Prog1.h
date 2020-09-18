c7afb6123bd6e873a5b54647c042637052e40804
Dmitriy Rodionov <57829161+Burgos1337@users.noreply.github.com>
2020-09-18 12:05:04 +0300

    resources added

--- Prog1/Prog1.h
+++ Prog1/Prog1.h
@@ -19,13 +19,13 @@ namespace Prog1 {
         return a > b;
     }
     
-    void Create_Node(struct Node**, double, int, int);
-    struct Node* Create_Matrix(int &, int &);
-    double Retrieve_Coordinates(struct Node*, int, int);
-    void Print_Matrix(const char*, struct Node*, int, int);
-    void Print_Vector(const char*, double [], int);
-    struct Node* Erase_Matrix(struct Node*);
-    double* Create_Vector(struct Node*, int, int);
-    double avg(struct Node*, int, int);
-    double max(struct Node*, int, int(*)(double, double));
+    void Create_Node(struct Node **, double, int, int);
+    struct Node *Create_Matrix(int &, int &);
+    double Retrieve_Coordinates(struct Node *, int, int);
+    void Print_Matrix(const char *, struct Node *, int, int);
+    void Print_Vector(const char *, double [], int);
+    struct Node *Erase_Matrix(struct Node *);
+    double *Create_Vector(struct Node *, int, int);
+    double avg(struct Node *, int, int);
+    double max(struct Node *, int, int(*)(double, double));
 }
